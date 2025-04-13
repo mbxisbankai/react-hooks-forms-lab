42bdccb85a3e88e2b5a58991977f06d9c2b60ac4
mbxisbankai <roy.mbui@student.moringaschool.com>
2025-04-13 20:42:31 +0300

    React Controlled Components Lab

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,40 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+
+function ShoppingList({ items, setItems }) {
+  
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [ typedItem, setTypedItem ] = useState("All");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function searchChange(event){
+    setTypedItem(event.target.value);
+  }
 
-    return item.category === selectedCategory;
+  function handleSubmit(newItem){
+    setItems((items) => [...items, newItem])
+  }
+
+
+
+  const itemsToDisplay = items.filter((item) => {
+    const categoryMatch = 
+      selectedCategory === "All" || item.category === selectedCategory;
+
+    const searchMatch = 
+      typedItem === "All" || item.name.toLowerCase().includes(typedItem.toLowerCase());
+
+    return categoryMatch && searchMatch;
   });
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm items={items} setItems={setItems} onItemFormSubmit={handleSubmit}/>
+      <Filter onSearchChange={searchChange} search={typedItem} onCategoryChange={handleCategoryChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
